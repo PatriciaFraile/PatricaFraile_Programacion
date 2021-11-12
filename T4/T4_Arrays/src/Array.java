1a54dc3e293b908276a08b6f9e851045fb9b3496
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2021-11-12 12:04:47 +0100

    update

--- T4/T4_Arrays/src/Array.java
+++ T4/T4_Arrays/src/Array.java
@@ -2,21 +2,31 @@ public class Array {
 
     public static void main(String[] args) {
         // Como crear una Arrays
-        int [] numeros = new int [10];
-        int [] numerosDos = {1,2,3,4,5,6,7,8,9,10};
+        int[] numeros = new int[10];
+        int[] numerosDos = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        boolean[] aciertos = new boolean [5];
-        String [] palabras = new String[3]; //nulo
+        boolean[] aciertos = new boolean[5];
+        String[] palabras = new String[3]; //nulo
 
-        double [] decimales = new double []{2.34,.23,34.5};
+        double[] decimales = new double[]{2.34, .23, 34.5};
 
 
         //Cuantos elementos tiene (length)
         int longitudArray = numerosDos.length;
         System.out.println(longitudArray);
         //Sacar todos los valores
-        for (int i = 0 ; i<=numerosDos.length-1;i++){
+        for (int i = 0; i < numerosDos.length; i++) {
             System.out.println(numerosDos[i]);
         }
+        //Modificar una posición concreta
+        numerosDos[7] = 88;
+
+        System.out.println("Modifico alguna posición ");
+
+        for (int i = 0; i < numerosDos.length; i++) {
+            System.out.println(numerosDos[i]);
+        }
+
+
     }
 }
