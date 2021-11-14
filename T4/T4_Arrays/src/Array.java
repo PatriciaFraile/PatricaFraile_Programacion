41e36eb5f6fb5c03af0dd0f4d2259ffa13e769d6
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2021-11-14 17:13:20 +0100

    update

--- T4/T4_Arrays/src/Array.java
+++ T4/T4_Arrays/src/Array.java
@@ -4,6 +4,8 @@ public class Array {
         // Como crear una Arrays
         int[] numeros = new int[10];
         int[] numerosDos = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int numeroPares=0;
+        int numerosImpares=0;
 
         boolean[] aciertos = new boolean[5];
         String[] palabras = new String[3]; //nulo
@@ -24,8 +26,17 @@ public class Array {
         System.out.println("Modifico alguna posición ");
 
         for (int i = 0; i < numerosDos.length; i++) {
-            System.out.println(numerosDos[i]);
+            numeros [i] = (int)(Math.random()*50)+1;
+            System.out.println("El número Aleatorio es:" +numeros[i]);
+            if((numeros[i]%2)==0){
+                numeroPares++;
+            } else {
+                numerosImpares++;
+            }
+
         }
+        System.out.println("La cantidad de números Pares son :"+ numeroPares);
+        System.out.println("La cantidad de números Impares son "+ numerosImpares);
 
 
     }
