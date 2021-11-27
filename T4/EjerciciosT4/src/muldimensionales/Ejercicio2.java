3a090ec1b223502b33cc62ad2d88f5f7d0415e25
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2021-11-27 17:58:06 +0100

    subir

--- T4/EjerciciosT4/src/muldimensionales/Ejercicio2.java
+++ T4/EjerciciosT4/src/muldimensionales/Ejercicio2.java
@@ -1,138 +1,148 @@
 package muldimensionales;
-
-
 import java.util.Scanner;
 public class Ejercicio2 {
 
     public static void main(String[] args) {
 
         Scanner entradaTeclado = new Scanner(System.in);
-        int alumnos =3;
-        int alumnosRegistrados = 0;
-        int nota1 = 0;
-        int nota2 = 0;
-        int nota3 = 0;
-        int notaMedia = 0;
-
-        Object [][] alumno = new Object [3][5];
-        alumno[0][0] = "Paula";
-        alumno [0][1] = nota1;
-        alumno [0][2] = nota2;
-        alumno [0][3] = nota3;
-        alumno[0][4] = notaMedia;
-
-        alumno[1][0] = "Alejandro";
-        alumno [1][1] = nota1;
-        alumno [1][2] = nota2;
-        alumno [1][3] = nota3;
-        alumno[1][4] = notaMedia;
-
-        alumno[2][0] = "Pablo";
-        alumno [2][1] = nota1;
-        alumno [2][2] = nota2;
-        alumno [2][3] = nota3;
-        alumno[2][4] = notaMedia;
-
-        nota1 = (int)(Math.random()*11);
-        nota2 = (int)(Math.random()*11);
-        nota3 = (int)(Math.random()*11);
-        notaMedia = (nota1+nota2+nota3)/3;
-
         int opcion;
-        do{
+        int numAlumnos = 0;
+        Object[][] alumno= new Object[1][1];
+        int sumaNotas = 0;
+        String alumnos = "";
+        double media = 0;
 
-            System.out.println("1.Definir el numero de registros");
-            System.out.println("2. Registrar alumnos");
-            System.out.println("3. Ver datos media de los alumnos");
-            System.out.println("4.Ver datos concretos de un alumno");
-            System.out.println("5. Ver media asignatura1");
-            System.out.println("6.Ver media asignatura2");
-            System.out.println("7.Ver media asignatura3");
-            System.out.println("Que opción quieres");
-            opcion= entradaTeclado.nextInt();
-            switch (opcion){
-                case 1 :
-                    System.out.println("Has elegido definir el numero de registros");
-                    System.out.println("Que numero de registros quieres");
-                    alumnosRegistrados = entradaTeclado.nextInt();
-                    if(alumnos==alumnosRegistrados){
-                        System.out.println("El numero registrado de alumnos es 3");
-                    }
-                    else if (alumnos!=alumnosRegistrados){
-                        System.out.println("Error");
+        do {
+            System.out.println("1.Definir el número de alumnos a registrar");
+            System.out.println("2.Registrar alumnos");
+            System.out.println("3.Ver datos de alumnos con nota media");
+            System.out.println("4.Ver datos de un alumno concreto");
+            System.out.println("5.Ver medias de la asignatura 1");
+            System.out.println("6.Ver medias de la asignatura 2");
+            System.out.println("7.Ver medias de la asignatura 3");
+            System.out.println("8.Salir");
+            opcion = entradaTeclado.nextInt();
+
+
+            switch (opcion) {
+                case 1:
+                    do {
+                        System.out.println("Numero de alumnos a registrar ");
+                        numAlumnos = entradaTeclado.nextInt();
+                        if (numAlumnos <= 1) {
+                            System.out.println("Error");
+                        } else {
+                            alumno = new Object[numAlumnos][4];
+                        }
+                    } while (numAlumnos <= 1);
+                    break;
+                case 2:
+                    if (numAlumnos <= 1) {
+                        System.out.println("Error. No hay alumnos registrados");
+                    } else if (alumno[1][1] != null) {
+                        System.out.println("Error. Ya hay alumnos registrados");
+                    } else {
+                        for (int i = 0; i < alumno.length; i++) {
+                            System.out.println("Introducir nombre de Alumno");
+                            alumno[i][0] = entradaTeclado.next();
+                        }
+                        for (int i = 0; i < alumno.length; i++) {
+                            System.out.println("Introducir nota asignatura 1 de " + alumno[i][0]);
+                            alumno[i][1] = entradaTeclado.nextInt();
+                        }
+                        for (int i = 0; i < alumno.length; i++) {
+                            System.out.println("Introducir nota asignatura 2 de " + alumno[i][0]);
+                            alumno[i][2] = entradaTeclado.nextInt();
+                        }
+                        for (int i = 0; i < alumno.length; i++) {
+                            System.out.println("Introducir nota asignatura 3 de " + alumno[i][0]);
+                            alumno[i][3] = entradaTeclado.nextInt();
+                        }
                     }
                     break;
-                case 2 :
-                    System.out.println("Has elegido registrar alumnos");
 
-                    if(alumno[0][0]==null){
-                        System.out.println("No hay alumnos registrados");
-                    }
-                    else if(alumno[0][0]!=null){
-                    System.out.println("Hay alumno registrado");
-                }
-                if(alumno[1][0]==null){
-                    System.out.println("No hay alumnos registrados");
-                }
-                else if(alumno[1][0]!=null){
-                    System.out.println("Hay alumno registrado");
-                }
-                if(alumno[2][0]==null){
-                    System.out.println("No hay alumnos registrados");
-                }
-                else if(alumno[2][0]!=null){
-                    System.out.println("Hay alumno registrado");
-                }
-                    break;
                 case 3:
-                    System.out.println("Has elegido ver nota media de los alumnos");
-                    String nombre = (String) alumno[0][0];
-                    System.out.println(nombre + " su nota media es " + notaMedia);
-                    String nombre2 = (String) alumno[1][0];
-                    System.out.println(nombre2 + " su nota media es " + notaMedia);
-                    String nombre3 = (String) alumno[2][0];
-                    System.out.println(nombre3 + " su nota media es " + notaMedia);
+                    if (numAlumnos <= 1) {
+                        System.out.println("Error. No hay alumnos registrados\n");
+                    } else {
+                        for (int i = 0; i < alumno.length; i++) {
+                            System.out.println(alumno[i][0]);
+                            System.out.println("Nota 1: " + alumno[i][1]);
+                            System.out.println("Nota 2: " + alumno[i][2]);
+                            System.out.println("Nota 3: " + alumno[i][3]);
+                            for (int j = 1; j < alumno[i].length; j++) {
+                                sumaNotas += (int) alumno[i][j];
+                            }
+                            System.out.printf("Nota media: %.2f", (double) sumaNotas / 3);
+                            System.out.println();
+                            sumaNotas = 0;
+                        }
+                    }
                     break;
                 case 4:
-                    String persona;
-                    System.out.println("Has elegido ver datos concretos de un alumno");
-                    System.out.println("Que alumno quieres ver");
-                    persona = entradaTeclado.nextLine();
+                    if (numAlumnos <= 1) {
+                        System.out.println("Error. No hay alumnos registrados\n");
+                    } else {
+                        System.out.println("De que alumno quieres ver los datos ");
+                        alumnos = entradaTeclado.next();
+                    }
                     for (int i = 0; i < alumno.length; i++) {
-                        for (int j = 3; j <= alumno.length; j++) {
-                            if (alumno[i][j].equals(persona)){
-                                System.out.println("Si esta registrado");
-
-
-                            }
-                            else{
-                                System.out.println("No esta registrado");
+                        for (int j = 0; j < 1; j++) {
+                            if (alumno[i][j].equals(alumnos)) {
+                                System.out.println("Nombre: " + alumno[i][0]);
+                                System.out.println("Nota 1: " + alumno[i][1]);
+                                System.out.println("Nota 2: " + alumno[i][2]);
+                                System.out.println("Nota 3: " + alumno[i][3]);
                             }
                         }
-                        
                     }
+                    break;
                 case 5:
-                    int mediaAsignatura1;
-                    mediaAsignatura1 = (nota1 + nota1 + nota1 )/3;
-                    System.out.println("Has elegido ver la media de la nota 1 ");
-                    System.out.println("La media es :" + mediaAsignatura1);
+                    if (numAlumnos <= 1) {
+                        System.out.println("Error. No hay alumnos registrados\n");
+                    } else {
+                        for (int i = 0; i < alumno.length; i++) {
+                            for (int j = 1; j < alumno[i].length - 2; j++) {
+                                media += (int) alumno[i][1];
+                            }
+                        }
+                        media /= alumno.length;
+                        System.out.println("La media de la asignatura 1 es: " + media);
+                        media = 0;
+                    }
                     break;
                 case 6:
-                    int mediaAsignatura2;
-                    mediaAsignatura2 = (nota2+nota2+nota2)/3;
-                    System.out.println("Has elegido ver la media de la nota 2 ");
-                    System.out.println("La media es :" + mediaAsignatura2);
+                    if (numAlumnos <= 1) {
+                        System.out.println("Error. No hay alumnos registrados\n");
+                    } else {
+                        for (int i = 0; i < alumno.length; i++) {
+                            for (int j = 2; j < alumno[i].length - 1; j++) {
+                                media += (int) alumno[i][2];
+                            }
+                        }
+                        media /= alumno.length;
+                        System.out.println("La media de la asignatura 2 es: " + media);
+                    }
+                    media = 0;
                     break;
                 case 7:
-                    int mediaAsignatura3;
-                    mediaAsignatura3 = (nota3+nota3+nota3)/3;
-                    System.out.println("Has elegido ver la media de la nota 2 ");
-                    System.out.println("La media es :" + mediaAsignatura3);
+                    if (numAlumnos <= 1) {
+                        System.out.println("Error. No hay alumnos registrados\n");
+                    } else {
+                        for (int i = 0; i < alumno.length; i++) {
+                            for (int j = 3; j < alumno[i].length; j++) {
+                                media += (int) alumno[i][3];
+                            }
+                        }
+                        media /= alumno.length;
+                        System.out.println("La media de la asignatura 3 es: " + media);
+                    }
+                    media = 0;
                     break;
                 default:
-                    System.out.println("Salir");
+                    System.out.println("Opcion no valida");
             }
-        }while(opcion<=7);
+        } while (opcion != 8);
         entradaTeclado.close();
 
     }
