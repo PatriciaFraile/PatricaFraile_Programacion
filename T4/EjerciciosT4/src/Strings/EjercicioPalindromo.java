cfa3500cc3fb234238032ff0ab27e676978c874c
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2021-12-03 12:55:31 +0100

    subir

---
+++ T4/EjerciciosT4/src/Strings/EjercicioPalindromo.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class EjercicioPalindromo {
+    public static void main(String[] args) {
+        String fraseCompleta = "Allí ve Sevilla";
+        boolean palindromo = false;
+        fraseCompleta = fraseCompleta.replaceAll(" ", "").toLowerCase();
+        for (int i = 0; i < (fraseCompleta.length() / 2); i++) {
+            if (fraseCompleta.charAt(i) == fraseCompleta.charAt(fraseCompleta.length() - 1 - i)) {
+                palindromo = true;
+            } else {
+                palindromo = false;
+                break;
+            }
+        }
+        if (palindromo) {
+            System.out.println("La palabra es palindromo");
+        } else {
+            System.out.println("La palabra no es palindromo");
+        }
+
+    }
+}
+
+
