5f7ca96d22fef418e4839c06e8ef2ea73d59e941
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-01-10 10:10:20 +0100

    UPDATE

--- T5/src/Entrada.java
+++
@@ -1,6 +0,0 @@
-public class Entrada {
-    String nombre = "Borja";
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ T5_POO/src/Entrada.java
@@ -0,0 +1,12 @@
+public class Entrada {
+    //ambito de clase
+    //String nombre = "Borja";
+
+    public static void main(String[] args) {
+        //ambito de metodo
+        //String nombre = "Borja";
+
+
+
+    }
+}
