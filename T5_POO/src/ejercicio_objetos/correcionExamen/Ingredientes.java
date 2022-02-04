8201dfc01e03e1a3352e5e2074b6371c615918e3
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-02-04 13:05:04 +0100

    update

---
+++ T5_POO/src/ejercicio_objetos/correcionExamen/Ingredientes.java
@@ -0,0 +1,27 @@
+package ejercicio_objetos.correcionExamen;
+
+public class Ingredientes {
+    private String nombre;
+    private double precio;
+
+    public Ingredientes(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
---
+++ T5_POO/src/ejercicio_objetos/correcionExamen/Pizzeria.java
@@ -0,0 +1,15 @@
+package ejercicio_objetos.correcionExamen;
+
+import java.util.ArrayList;
+
+public class Pizzeria {
+    private ArrayList <Pizza> listaPizzas;
+    private double caja;
+    public Pizzeria(){
+        //caja = 0;
+        listaPizzas = new ArrayList<>();
+    }
+    public void agregarPedido(Pizza pizza){
+        this.listaPizzas.add(pizza);
+    }
+}
