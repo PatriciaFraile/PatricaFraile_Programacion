ca4ead7a1539df3e9f23a105cc0235899fbfb61c
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-02-11 11:47:10 +0100

    subir

--- T6/src/Concesionario.java
+++ T6/src/Concesionario.java
@@ -1,4 +1,64 @@
+import java.util.ArrayList;
+
 public class Concesionario {
-    //incompleto
+    ArrayList<Coche> listaCoches;
+
+    public Concesionario() {
+        listaCoches = new ArrayList<>();
+    }
+
+    public void agregarCoche(Coche coche) {
+        listaCoches.add(coche);
+    }
+
+    public void buscarCoches(String matricula) {
+        boolean encontrado = false;
+        for (Coche item : listaCoches) {
+            if (matricula.equals(item.getMatricula())) {
+                encontrado = true;
+                System.out.println("Matricula\t" + item.getMatricula());
+                System.out.println("Bastidor\t" + item.getBastidor());
+                System.out.println("CV\t" + item.getCv());
+                System.out.println("CC\t" + item.getCc());
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Coche no encontrado");
+        }
+
+    }
+
+    public void borrarCoche(String matricula) {
+        boolean encontrado = false;
+        for (int i = 0; i < listaCoches.size(); i++) {
+            if (listaCoches.get(i).getMatricula().equalsIgnoreCase(matricula)) {
+                System.out.println("Coche encontrado");
+                encontrado = true;
+                System.out.println("Tipo de coche es");
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Coche no encontrado");
+        }
+
+
+    }
+
+
+    public void clasificarCoche(String matricula) {
+        boolean encontrado = false;
+        for (Coche item : listaCoches) {
+            if (matricula.equals(item.getMatricula())) {
+                encontrado = true;
+                System.out.println("Matricula\t" + item.getMatricula());
+                System.out.println("Bastidor\t" + item.getBastidor());
+                System.out.println("CV\t" + item.getCv());
+                System.out.println("CC\t" + item.getCc());
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Coche no encontrado");
+        }
+    }
 
 }
--- T6/src/Entrada.java
+++ T6/src/Entrada.java
@@ -1,23 +1,42 @@
+import java.util.Scanner;
+
 public class Entrada {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Concesionario concesionario = new Concesionario();
         Coche coche = new Coche("123A", "123", 100, 20000);
         coche.calcularVelocidad(80);
         System.out.println(coche.getVelocidad());
+        System.out.println("Coche agregado");
+        concesionario.agregarCoche(coche);
         Deportivo deportivo = new Deportivo("234B", "234", 200, 2500, 600);
         System.out.println("Deportivo");
         deportivo.calcularVelocidad(678);
         System.out.println(deportivo.getVelocidad());
+        System.out.println("Deportivo agregado");
+        concesionario.agregarCoche(deportivo);
         deportivo.mostrarDatos();
         Electrico electrico = new Electrico("2345", "222", 200, 300, 400);
         System.out.println("Electrico");
         electrico.calcularVelocidad(1234);
         System.out.println(electrico.getVelocidad());
         electrico.mostrarDatos();
+        System.out.println("Electrico agregado");
+        concesionario.agregarCoche(electrico);
         Suv suv = new Suv("2345", "3456", 455, 678, true);
         System.out.println("Suv");
         suv.calcularVelocidad(89);
         System.out.println(suv.getVelocidad());
         suv.mostrarDatos();
+        System.out.println("Suv agregado");
+        concesionario.agregarCoche(suv);
+        System.out.println("Que coche quieres buscar");
+        String matricula = entrada.next();
+        concesionario.buscarCoches(matricula);
+        System.out.println("Que coche quieres borrar");
+        String matricula2 = entrada.next();
+        concesionario.borrarCoche(matricula2);
+
 
     }
 }
