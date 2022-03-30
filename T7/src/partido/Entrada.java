f2bebdfc3052aa0c2c8bd23094fc9892e7af5d78
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-03-30 17:40:08 +0200

    subir

---
+++ T7/src/partido/Entrada.java
@@ -0,0 +1,19 @@
+package partido;
+
+public class Entrada {
+    public static void main(String[] args) {
+        Partido partido = new Partido();
+        Partido.Equipo madrid = partido.new Equipo("Madrid","Local");
+        Partido.Equipo barcelona = partido.new Equipo("Barcelona","Visitante");
+
+        partido.mostrarEquipos();
+        partido.mostrarResultado();
+        for (int i = 0; i <3 ; i++) {
+            partido.jugarPartido();
+            partido.mostrarResultado();
+            partido.calcularPuntos();
+        }
+        madrid.mostrarInformacion();
+        barcelona.mostrarInformacion();
+    }
+}
