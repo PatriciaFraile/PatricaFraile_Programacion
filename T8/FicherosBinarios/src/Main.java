60a05d02c24447adbe91cdfcc5d5bdae75ae2b6e
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-04-21 10:14:32 +0200

    update

--- T8/FicherosBinarios/src/Main.java
+++ T8/FicherosBinarios/src/Main.java
@@ -3,7 +3,7 @@ import controller.BinariosController;
 public class Main {
     public static void main(String[] args) {
         BinariosController controller = new BinariosController();
-        //controller.escribirBinario();
+        controller.escribirBinario();
         controller.lecturaBiniario();
 
     }
