59a990f967dc04f67f40ff53be5a3cba19463496
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-05-03 10:21:24 +0200

    subir

---
+++ T8/FicherosJSON/Ejercicio1/src/main/java/Entrada.java
@@ -0,0 +1,16 @@
+import controller.JSONController;
+
+import java.util.Scanner;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+        JSONController controller = new JSONController();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Genero");
+        String genero = entrada.nextLine();
+
+
+
+    }
+}
---
+++ T8/FicherosJSON/Ejercicio2/src/main/java/controller/JSONController.java
@@ -0,0 +1,50 @@
+package controller;
+import org.json.*;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class JSONController {
+
+
+    public void lecturaUrl(){
+        String urlString = "https://www.thesportsdb.com/api/v1/json/2/all_leagues.php";
+        URL url = null;
+
+            try {
+                url = new URL(urlString);
+                HttpURLConnection connection = null;
+                connection = (HttpURLConnection) url.openConnection();
+                BufferedReader bufferedReader;
+                bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuffer stringBuffer =new StringBuffer();
+                String linea = null;
+                while ((linea=bufferedReader.readLine())!= null){
+                    stringBuffer.append(linea);
+                }
+                String response = stringBuffer.toString();
+                String response = stringBuffer.toString();
+                JSONObject responseJSON = new JSONObject(response);
+                JSONArray arrayResultados = responseJSON.getJSONArray("results");
+
+                for (int i = 0; i < arrayResultados.length(); i++) {
+                    JSONObject objetoResultado = arrayResultados.getJSONObject(i);
+                    //System.out.println(objetoResultado);
+                    String mail = objetoResultado.getString("email");
+                    System.out.println(mail);
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+    }
+}
